9a91b658db736ae1d30436ab7f57aa1b05c454db
Mikhail-bmstu <solovei.0807@gmail.com>
2022-01-30 18:19:40 +0300

    add images to README.md

--- main.py
+++ main.py
@@ -39,10 +39,10 @@ def composite_criterion(data):
     d = sum(list(map(lambda x: abs(x - mean), data))) / (n * sigma_offset)
     print("d: ", d)
 
-    d_min, d_max = map(float, input(f"Enter d_min and d_max for n = {n}(look appendix table 5): ").split())
+    d_min, d_max = map(float, input(f"Enter the d_min and d_max for n = {n} (look appendix table 5): ").split())
 
     if d_min < d < d_max:
-        z, m_table = map(float, input(f"Enter table values \"z\" and \"m\" for n = {n}(look appendix table 6): ").split())
+        z, m_table = map(float, input(f"Enter the table values \"z\" and \"m\" for n = {n} (look appendix table 6): ").split())
         m = sum(list(map(lambda x: 1 if abs(mean - x) > z * sigma else 0, data)))
 
         if m < m_table:
@@ -54,62 +54,66 @@ def composite_criterion(data):
 
 
 def main():
-    print("1 - Miss detection")
-    print("2 - Composite criterion (definition of the distribution law)")
-    print("3 - Multiple measurements (includes removal of misses))")
-    print("4 - Output mean, standard deviation, offset standard deviation, mean standard deviation")
-    mode = int(input("Choose mod: "))
-    data = list(map(float, input("Enter the data(separated by a space):\n").split()))
+    mode = 0
+    while mode < 5:
+        print("1 - Miss detection")
+        print("2 - Composite criterion (definition of the distribution law)")
+        print("3 - Multiple measurements (includes removal of misses))")
+        print("4 - Output mean, standard deviation, offset standard deviation, mean standard deviation")
+        print("5 - Exit")
+        mode = int(input("Choose mod: "))
+        if mode > 4:
+            break
+        data = list(map(float, input("Enter the data(separated by a space):\n").split()))
 
-    if mode == 1:  # misses
-        print("data before:", *data)
-        print("Misses:")
-        print("data after:", *lose(data))
+        if mode == 1:  # misses
+            print("data before:", *data)
+            print("Misses:")
+            print("data after:", *lose(data))
 
-    if mode == 2:  # composite criterion
-        if len(data) < 15:
-            print("Belonging to the normal distribution law isn't checked")
-        elif composite_criterion(data):
-            print("The data doesn't belong to the normal distribution law")
-        else:
-            print("The data belong to the normal distribution law")
+        if mode == 2:  # composite criterion
+            if len(data) < 15:
+                print("Belonging to the normal distribution law isn't checked")
+            elif composite_criterion(data):
+                print("The data doesn't belong to the normal distribution law")
+            else:
+                print("The data belong to the normal distribution law")
 
-    if mode == 3:  # multiple measurements
-        print("data before:", *data)
-        print("Misses:")
-        data = lose(data)
-        print("data after:", *data)
+        if mode == 3:  # multiple measurements
+            print("data before:", *data)
+            print("Misses:")
+            data = lose(data)
+            print("data after:", *data)
 
-        if composite_criterion(data):
-            print("The data doesn't belong to the normal distribution law")
-        else:
-            print("The data belong to the normal distribution law")
+            if composite_criterion(data):
+                print("The data doesn't belong to the normal distribution law")
+            else:
+                print("The data belong to the normal distribution law")
 
-        n = len(data)
-        mean = np.mean(data)
+            n = len(data)
+            mean = np.mean(data)
 
-        sigma = standard_deviation(data)
-        sigma_mean = sigma / n ** 0.5
-        print("data:", *data)
-        print("n:", n)
-        print("mean:", mean)
-        print("standard deviation:", sigma)
-        print("mean standard deviation:", sigma_mean)
+            sigma = standard_deviation(data)
+            sigma_mean = sigma / n ** 0.5
 
-    if mode == 4:
-        n = len(data)  # count of measurements
-        mean = np.mean(data)
-        sigma = standard_deviation(data)
-        sigma_offset = offset_standard_deviation(data)
-        # mean standard deviation = sigma/sqrt(n)
-        sigma_mean = sigma / n ** 0.5
+            print("n:", n)
+            print("mean:", mean)
+            print("standard deviation:", sigma)
+            print("mean standard deviation:", sigma_mean)
 
-        print("data:", *data)
-        print("n:", n)
-        print("mean:", mean)
-        print("standard deviation:", sigma)
-        print("offset standard deviation:", sigma_offset)
-        print("mean standard deviation:", sigma_mean)
+        if mode == 4:
+            n = len(data)  # count of measurements
+            mean = np.mean(data)
+            sigma = standard_deviation(data)
+            sigma_offset = offset_standard_deviation(data)
+            # mean standard deviation = sigma/sqrt(n)
+            sigma_mean = sigma / n ** 0.5
+
+            print("n:", n)
+            print("mean:", mean)
+            print("standard deviation:", sigma)
+            print("offset standard deviation:", sigma_offset)
+            print("mean standard deviation:", sigma_mean)
 
 
 if __name__ == "__main__":
